a1a7231142364374d13f896bfae2d6588d0ffcc6
Jonathan Chasteen <jonathan.chasteen@live.com>
2024-05-16 11:58:49 -0400

    Add eg_data.txt to templates

--- GeneralPurpose/BarcodeListulator/build.js
+++ GeneralPurpose/BarcodeListulator/build.js
@@ -8,7 +8,8 @@ const read_opt = { encoding: "utf-8" };
 $(() => {
     const t = Template(path.join(__dirname, "template.html"), {
         STYLE: fs.readFileSync(path.join(__dirname, "style.css"), read_opt),
-        SCRIPT: fs.readFileSync(path.join(__dirname, "main.js"), read_opt)
+        SCRIPT: fs.readFileSync(path.join(__dirname, "main.js"), read_opt),
+        EGDATA: fs.readFileSync(path.join(__dirname, "eg_data.txt"), read_opt),
     });
 
     t.write(path.join(__dirname, "BarcodeListulator.html"));
